b9a41727f32c05a34023bdc8364e4ee60143d2db
jennysival <jeniffersival@gmail.com>
2025-11-16 19:02:50 -0300

    refactor(Repository): refactor Repository, Mapper and DI to use Database and improve coding

--- app/src/main/java/com/picpay/desafio/android/MainApplication.kt
+++ app/src/main/java/com/picpay/desafio/android/MainApplication.kt
@@ -1,7 +1,11 @@
 package com.picpay.desafio.android
 
 import android.app.Application
-import com.picpay.desafio.android.di.appModule
+import com.picpay.desafio.android.di.databaseModule
+import com.picpay.desafio.android.di.networkModule
+import com.picpay.desafio.android.di.repositoryModule
+import com.picpay.desafio.android.di.useCaseModule
+import com.picpay.desafio.android.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -13,7 +17,13 @@ class MainApplication : Application(){
         startKoin{
             androidLogger()
             androidContext(this@MainApplication)
-            modules(appModule)
+            modules(
+                networkModule,
+                databaseModule,
+                repositoryModule,
+                useCaseModule,
+                viewModelModule
+                )
         }
     }
 }
--- app/src/main/java/com/picpay/desafio/android/di/AppModule.kt
+++ app/src/main/java/com/picpay/desafio/android/di/AppModule.kt
@@ -1,20 +1,72 @@
 package com.picpay.desafio.android.di
 
+import androidx.room.Room
+import com.google.gson.GsonBuilder
+import com.picpay.desafio.android.data.local.UserDao
+import com.picpay.desafio.android.data.local.UserDatabase
+import com.picpay.desafio.android.data.remote.PicPayService
 import com.picpay.desafio.android.data.repository.UserRepositoryImpl
 import com.picpay.desafio.android.domain.mapper.UserDtoMapper
 import com.picpay.desafio.android.domain.repository.UserRepository
 import com.picpay.desafio.android.domain.usecase.GetUsersUseCase
 import com.picpay.desafio.android.ui.user.viewModel.UserViewModel
+import okhttp3.OkHttpClient
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-val appModule = module {
+val networkModule = module {
+    val baseUrl = "https://609a908e0f5a13001721b74e.mockapi.io/picpay/api/"
 
+    single {
+        OkHttpClient.Builder().build()
+    }
+
+    single {
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(get())
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+    }
+
+    single<PicPayService> {
+        get<Retrofit>().create(PicPayService::class.java)
+    }
+}
+
+val databaseModule = module {
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            UserDatabase::class.java,
+            "users_db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    single<UserDao> {
+        val database = get<UserDatabase>()
+        database.userDao()
+    }
+}
+
+val repositoryModule = module {
     single { UserDtoMapper() }
+    single<UserRepository> { UserRepositoryImpl(
+        userMapper = get(),
+        apiService = get(),
+        userDao = get()
+    ) }
+}
 
-    single<UserRepository> { UserRepositoryImpl(userMapper = get()) }
-
+val useCaseModule = module {
     factory { GetUsersUseCase(userRepository = get()) }
+}
 
+val viewModelModule =  module {
     viewModel { UserViewModel(useCase = get()) }
 }
