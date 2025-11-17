08ee81cbcad4e8812f730658a580c5d6d16b8d02
jennysival <jeniffersival@gmail.com>
2025-11-16 21:01:41 -0300

    test(UseCase): add unit tests

--- app/src/test/java/com/picpay/desafio/android/domain/mapper/UserDtoMapperTest.kt
+++ app/src/test/java/com/picpay/desafio/android/domain/mapper/UserDtoMapperTest.kt
@@ -1,44 +1,14 @@
 package com.picpay.desafio.android.domain.mapper
 
-import com.picpay.desafio.android.data.local.UserDbModel
-import com.picpay.desafio.android.data.remote.model.User
-import com.picpay.desafio.android.domain.model.UserModel
+import com.picpay.desafio.android.userApiList
+import com.picpay.desafio.android.userDbModelList
+import com.picpay.desafio.android.userModelList
 import org.junit.Assert.*
 import org.junit.Test
 
 class UserDtoMapperTest {
 
     private val subject = UserDtoMapper()
-
-    private val stringTest = "test"
-    private val intTest = 1
-
-    private val userApiList = listOf(
-        User(
-            img = stringTest,
-            name = stringTest,
-            id = intTest,
-            username = stringTest
-        )
-    )
-
-    private val userDbModelList = listOf(
-        UserDbModel(
-            img = stringTest,
-            name = stringTest,
-            id = intTest,
-            username = stringTest
-        )
-    )
-
-    private val userModelList = listOf(
-        UserModel(
-            img = stringTest,
-            name = stringTest,
-            id = intTest,
-            username = stringTest
-        )
-    )
 
     @Test
     fun `when mapCachedUsersModelList is called should return a UserModel list `() {
