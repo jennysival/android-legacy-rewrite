25b3150899f2eb7d9ac75637c1d772a5b92a3cde
jennysival <jeniffersival@gmail.com>
2025-11-16 21:26:45 -0300

    test(ViewModel): add unit tests

---
+++ app/src/test/java/com/picpay/desafio/android/ui/user/viewModel/UserViewModelTest.kt
@@ -0,0 +1,68 @@
+package com.picpay.desafio.android.ui.user.viewModel
+
+import androidx.lifecycle.Observer
+import com.picpay.desafio.android.domain.model.UserModel
+import com.picpay.desafio.android.domain.usecase.GetUsersUseCase
+import com.picpay.desafio.android.testRulesCoroutinesLiveData
+import com.picpay.desafio.android.ui.ViewState
+import com.picpay.desafio.android.userModelList
+import io.mockk.Runs
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verifyOrder
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class UserViewModelTest {
+
+    private val useCaseMock = mockk<GetUsersUseCase>()
+    private val subject = UserViewModel(useCaseMock)
+    private val userListObserver = mockk<Observer<ViewState<List<UserModel>>>>()
+
+    @get:Rule
+    val rule = testRulesCoroutinesLiveData
+
+    @Before
+    fun setUp() {
+        every { userListObserver.onChanged(any()) } just Runs
+    }
+
+    @After
+    fun tearDown() {
+        subject.userListState.removeObserver(userListObserver)
+    }
+
+    @Test
+    fun `userListState should be Loading then Success when useCase returns a list`() {
+        runBlocking {
+            coEvery { useCaseMock.getUsers() } returns userModelList
+            subject.userListState.observeForever(userListObserver)
+
+            subject.getUsers()
+
+            verifyOrder {
+                userListObserver.onChanged(ViewState.Loading)
+                userListObserver.onChanged(ViewState.Success(userModelList))
+            }
+        }
+    }
+
+    @Test
+    fun `userListState should be Loading then Error when useCase throws an exception`() {
+        val exception = RuntimeException("Network Error")
+        coEvery { useCaseMock.getUsers() } throws exception
+        subject.userListState.observeForever(userListObserver)
+
+        subject.getUsers()
+
+        verifyOrder {
+            userListObserver.onChanged(ViewState.Loading)
+            userListObserver.onChanged(ViewState.Error(exception))
+        }
+    }
+}
